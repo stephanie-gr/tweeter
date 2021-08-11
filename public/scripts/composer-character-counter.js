c88942c563d4531d1569861c19c82788e3943411
Stephanie <stephanie.grant.2008@gmail.com>
2021-08-11 01:16:57 +0000

    created and implemented composer-character-counter

---
+++ public/scripts/composer-character-counter.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+  console.log('I, the DOM, am ready!');
+
+  //create a new jQuery object for the textarea input(grabbing it by its ID)
+  const $newTweetInput = $('#tweet-text');
+
+  $newTweetInput.on('input', function() {
+    //target the output field (the counter) and save it to a variable 
+    const $counter = $(this).parent().find('output');
+    
+    //declare number of characters (140); save length of value of textarea input to a variable
+    let numOfCharacters = this.value.length;
+    let charactersLeft = 140;
+    
+    //subtract numOfCharacters from charactersLeft and set charactersLeft to be counter value
+    charactersLeft -= numOfCharacters;
+    $( $counter ).val( charactersLeft );
+    
+    //make sure characters left and counter reflect that you are over-limit with negative, red number
+    if (charactersLeft < 0) {
+      charactersLeft = 140 - numOfCharacters;
+      return $( $counter ).css('color', 'red');
+    }
+
+    return $( $counter ).css('color', 'black');
+    
+  })
+
+
+    
+    
+    
+    
+
+
+  // const $newTweetButton = $('.submit-button');
+
+  // $newTweetButton.on('click', () =>{
+  //   const $newTweetValue = $newTweetInput.val();
+  //   const $newTweet = $('<li>').text($newTweetValue);
+  // })
+});
+
